fd946381cf8ea9177835a3fa035911a33aeb5821
Brahman101 <127399156+Brahman101@users.noreply.github.com>
2024-07-22 10:42:17 +0700

    change part1

---
+++ resources/views/newhome.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JDIH Kota Wisata Batu</title>
+    <link rel="stylesheet" href="{{ asset('css/newberanda.css') }}">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <!-- Navigation -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary navbar-kesatu">
+        <div class="container-navbar">
+            <img src="{{ asset('images/LogoKotaBatu.png') }}" alt="Logo Kota Batu" class="logo-batu">
+            <img src="{{ asset('images/ikon_bangunan.png') }}" alt="Alamat" class="ikon-topnav">
+            <p>Bagian Hukum Setda. Kota Wisata Batu</p>
+            <img src="{{ asset('images/ikon_mail.png') }}" alt="E-mail" class="ikon-topnav">
+            <p>hukumkwb@gmail.com</p>
+            <img src="{{ asset('images/ikon_telepon.png') }}" alt="NoTelp" class="ikon-topnav">
+            <p>(0341) 591032</p>
+        </div>
+    </nav>
+
+    <!-- Logo & Title -->
+    <header class="web-intro">
+        <div class="container text-center logo-title">
+            <div class="row justify-content-center logo">
+                <img src="{{}}" alt="">
+            </div>
+        </div>
+
+    </header>
+</body>
+
+</html>
