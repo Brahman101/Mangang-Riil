436ef860834c3530dd33449df556eb8449f955d4
Brahman101 <127399156+Brahman101@users.noreply.github.com>
2024-07-22 09:03:53 +0700

    First Commit

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/css/style.css">
+</head>
+
+<body>
+    <!-- Top Details -->
+    <div class="topnav">
+        <img src="/assets/images/LogoKotaBatu.png" alt="Logo Kota Batu" class="logo-batu">
+        <img src="/assets/icons/ikon_bangunan.png" alt="Alamat" class="ikon-topnav">
+        <p>Bagian Hukum Setda. Kota Wisata Batu</p>
+        <img src="/assets/icons/ikon_mail.png" alt="E-mail" class="ikon-topnav">
+        <p>hukumkwb@gmail.com</p>
+        <img src="/assets/icons/ikon_telepon.png" alt="NoTelp" class="ikon-topnav">
+        <p>(0341) 591032</p>
+    </div>
+
+    <!-- Logo & Title -->
+    <div class="container-logo-title">
+        <div class="logo-container">
+            <img src="/assets/images/LogoJDIH.png" alt="Logo JDIH" class="logo-in-container">
+            <img src="/assets/images/LogoKotaBatu.png" alt="Logo Kota Batu" class="logo-in-container">
+        </div>
+        <div class="title-container">
+            <h1>JDIH Kota Wisata Batu</h1>
+            <h2>Jaringan Dokumentasi dan Informasi Hukum Kota Wisata Batu</h2>
+        </div>
+    </div>
+
+    <!-- Navbar -->
+    <div class="navbar">
+        <a href=""> Beranda</a>
+        <!-- dropdown -->
+        <a href=""> Produk Hukum</a>
+        <a href=""> Berita</a>
+        <!-- dropdown -->
+        <a href=""> Statistik</a>
+        <a href=""> Forum</a>
+        <!-- dropdown -->
+        <a href=""> Profil</a>
+        <!-- dropdown -->
+        <a href=""> Informasi Hukum</a>
+        <a href=""> Media</a>
+        <a href=""> FAQ</a>
+        <a href=""> Kontak</a>
+    </div>
+
+
+    <!-- Search Document -->
+    <div class="search-doc">
+        <div class="search-container-1">
+            <h3>Cari Produk Hukum</h3>
+            <form action="">
+                <input type="query-text" placeholder="Cari..">
+            </form>
+        </div>
+        <div class="search-container-2"></div>
+        <div class="search-container-3"></div>
+    </div>
+</body>
+
+</html>
